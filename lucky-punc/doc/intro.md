0f919698ae79a421dcd6c6d963621bca257989d5
Lanzafame <adrianlanzafame92@gmail.com>
2015-04-18 09:51:00 +1000

    Added clojure project

---
+++ lucky-punc/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lucky-punc
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ lucky-punc/test/lucky_punc/core_test.clj
@@ -0,0 +1,7 @@
+(ns lucky-punc.core-test
+  (:require [clojure.test :refer :all]
+            [lucky-punc.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
